0dec5bb063b47aca12ebda60c0af821ffcd1b139
Param Singh <me@param.codes>
2023-07-26 21:46:13 +0530

    set as unordered list

---
+++ sets/unordered-list.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? (car set) x) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2) (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))
+        ))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((not (element-of-set? (car set1) set2)) (union-set (cdr set1) (cons (car set1) set2)))
+        (else (union-set (cdr set1) set2))
+        ))
+
+(println (union-set '(1 2 3) '(4 5 6)))
+(println (union-set '(1 2 3) '(1 3 6)))
