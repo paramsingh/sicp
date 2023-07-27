bdcf18b42904c7464e726f84dbd1cc8e94e52007
Param Singh <me@param.codes>
2023-07-27 17:07:46 +0530

    MIT project 1

---
+++ sets/ordered-list.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(define (element-of-set? x set)
+  (cond
+    ((null? set) false)
+    ((= x (car set)) true)
+    ((< x (car set)) false)
+    (else (element-of-set? x  (car set)))
+    ))
