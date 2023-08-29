4f4f0505547c9e3ab85a747f6f3bd2e7da44c97c
Param Singh <me@param.codes>
2023-08-29 19:54:33 +0530

    3.14

---
+++ chapter-3/exercises/3.14.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(mystery (list 1 2 3))
