c0c75d12b1efa36080e5eedcc98596f714121b89
Param Singh <me@param.codes>
2023-08-30 14:40:02 +0530

    3.19

---
+++ chapter-3/exercises/3.19.rkt
@@ -0,0 +1,25 @@
+#lang sicp
+
+
+;;; Exercise 3.19: Redo Exercise 3.18 using an algorithm that
+;;; takes only a constant amount of space. (This requires a very
+;;; clever idea.)
+
+(define (last-pair x)
+  (if (null? (cdr x)) x (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+(define (cycle? lst)
+  (define (helper slow fast)
+    (cond ((eq? slow fast) true)
+          ((or (null? fast) (null? (cdr fast))) false)
+          (else (helper (cdr slow) (cdr (cdr fast))))))
+  (helper (cdr lst) (cdr (cdr lst))))
+
+(cycle? z)
+(cycle? (list 1 2 3 4))
