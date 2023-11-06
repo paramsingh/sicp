387dfce047e4523af5faa5dddeeb8e0efc7e250f
Param Singh <me@param.codes>
2023-11-06 17:06:41 +0530

    Some helpers to test easily

--- chapter-4/lazy-evaluator.rkt
+++ chapter-4/lazy-evaluator.rkt
@@ -290,16 +290,14 @@
 
 
 (define primitive-procedures
-  (list (list 'car car)
-        (list 'cdr cdr)
-        (list 'cons cons)
-        (list 'null? null?)
-        (list 'eq? eq?)
-        (list '+ +)
-        (list '* *)
-        (list '= =)
-        (list '- -)
-        (list 'list list)))
+  (list
+   (list 'null? null?)
+   (list 'eq? eq?)
+   (list '+ +)
+   (list '* *)
+   (list '= =)
+   (list '- -)
+   (list 'list list)))
 
 (define (primitive-procedure-names)
   (map car primitive-procedures))
@@ -347,3 +345,27 @@
 
 (define the-global-environment (setup-environment))
 (driver-loop)
+
+;;; THESE ARE DEFINITIONS TO PUT IN TO TEST THE LAZY
+;;; EVALUATION:
+;;; TODO: Put these in the environment by default
+;;; (define (cons x y) (lambda (m) (m x y)))
+;;; (define (car z) (z (lambda (p q) p)))
+;;; (define (cdr z) (z (lambda (p q) q)))
+;;; (define (list-ref items n)
+;;;   (if (= n 0)
+;;;       (car items)
+;;;       (list-ref (cdr items) (- n 1))))
+;;; (define (map proc items)
+;;;   (if (null? items)
+;;;       '()
+;;;       (cons (proc (car items)) (map proc (cdr items)))))
+;;; (define (scale-list items factor)
+;;;   (map (lambda (x) (* x factor)) items))
+;;; (define (add-lists list1 list2)
+;;;   (cond ((null? list1) list2)
+;;;         ((null? list2) list1)
+;;;         (else (cons (+ (car list1) (car list2))
+;;;                     (add-lists (cdr list1) (cdr list2))))))
+;;; (define ones (cons 1 ones))
+;;; (define integers (cons 1 (add-lists ones integers)))
